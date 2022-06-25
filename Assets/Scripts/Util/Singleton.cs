7fefce24e8bda6568796578eed7c4b3a61c93d7a
berke <bugdayberke@gmail.com>
2022-06-25 22:50:12 +0300

    refactor singleton class

--- Assets/Scripts/Util/Singleton.cs
+++ Assets/Scripts/Util/Singleton.cs
@@ -2,30 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Singleton<T> : MonoBehaviour where T : Component
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
 {
-    private static T instance;
-    public static T Instance {
-      get {
-        if (instance == null) {
-           instance = FindObjectOfType<T> ();
-           if (instance == null) {
-             GameObject obj = new GameObject ();
-             obj.name = typeof(T).Name;
-             instance = obj.AddComponent<T>();
-           }
-        }
-       return instance;
-      }
-    }
- 
-    public virtual void Awake ()
-    {
-       if (instance == null) {
-         instance = this as T;
-         DontDestroyOnLoad (this.gameObject);
-    } else {
-      Destroy (gameObject);
-    }
-  }
+	private static T instance = null;
+	public static T Instance 
+	{
+    	get 
+    	{
+      		if (instance == null) 
+      		{
+          		instance = FindObjectOfType<T> ();
+          		if (instance == null) 
+          		{
+					GameObject obj = new GameObject ();
+					obj.name = typeof(T).Name;
+					instance = obj.AddComponent<T>();
+          		}
+			}
+			return instance;
+		}
+	}
+
+	public virtual void Awake ()
+  	{
+		if (instance != null) 
+		{
+			Destroy (gameObject);
+			return;
+		}
+
+		instance = GetComponent<T>();
+
+		DontDestroyOnLoad(gameObject);
+
+		if(instance == null)
+			return;
+	}
 }
