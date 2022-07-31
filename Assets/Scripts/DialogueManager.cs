0efa7de91e3322826ec20aa95996e132e227d3f9
berke <bugdayberke@gmail.com>
2022-07-31 16:31:35 +0300

    display choices

--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -3,15 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using Ink.Runtime;
+using TMPro;
 
 
 //use singleton??
 public class DialogueManager : Singleton<DialogueManager>
 {
 
+    //bool dialogueIsPlaying? to handle inputs
+
     [SerializeField] GameObject dialoguePanel;
     
     [SerializeField] Text dialogueText;
+
+    [SerializeField] private GameObject[] choices;
+    private TextMeshProUGUI[] choicesText;
 
     Story currentStory;
 
@@ -23,15 +29,53 @@ public class DialogueManager : Singleton<DialogueManager>
     // Start is called before the first frame update
     void Start()
     {
-        
+        dialoguePanel.SetActive(false);
+
+        choicesText = new TextMeshProUGUI[choices.Length];
+        int index = 0;
+        foreach (GameObject choice in choices) 
+        {
+            choice.SetActive(false);
+            choicesText[index] = choice.GetComponentInChildren<TextMeshProUGUI>();
+            index++;
+        }
     }
 
     public void HandleUpdate()
     {
-        if(Input.GetKeyDown(KeyCode.Z))
-            ContinueStory();
+        if(Input.GetKeyDown(KeyCode.Z)){
+            if(currentStory.currentChoices.Count == 0)
+                ContinueStory();
+            else
+                DisplayChoices();
+        }
     }
 
+    private void DisplayChoices()
+    {
+        List<Choice> currentChoices = currentStory.currentChoices;
+        if (currentChoices.Count > choices.Length)
+        {
+            Debug.LogError("More choices were given than the UI can support. Number of choices given: " 
+                + currentChoices.Count);
+        }
+
+        int index = 0;
+
+        // enable and initialize the choices up to the amount of choices for this line of dialogue
+        
+        foreach(Choice choice in currentChoices) 
+        {
+            choices[index].gameObject.SetActive(true);
+            choicesText[index].text = choice.text;
+            index++;
+        }
+
+        for (int i = index; i < choices.Length; i++) 
+        {
+            choices[i].gameObject.SetActive(false);
+        }
+    }
 
     public void ShowInkDialog(TextAsset inkJSON)
     {
