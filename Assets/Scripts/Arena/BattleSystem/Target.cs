4d0de611a2c7667cb9e3ede6ae98b463552ea9d6
berke <bugdayberke@gmail.com>
2022-08-30 21:27:21 +0300

    damage shader effect

--- Assets/Scripts/Arena/BattleSystem/Target.cs
+++ Assets/Scripts/Arena/BattleSystem/Target.cs
@@ -4,13 +4,34 @@ using UnityEngine;
 
 public abstract class Target : BattleEntity
 {
+    private SpriteRenderer myRenderer;
+    private Shader shaderGUItext;
+    private Shader shaderSpritesDefault;
+
     DamagePopup damagePopup;
 
     bool stackDamageOn = false;
     int stackedDamage = 0;
 
+    void Awake()
+    {
+        myRenderer = gameObject.GetComponent<SpriteRenderer>();
+        shaderGUItext = Shader.Find("GUI/Text Shader");
+        shaderSpritesDefault = Shader.Find("Sprites/Default");
+    }
+
+    private IEnumerator WhiteSpriteEffect()
+    {
+        myRenderer.material.shader = shaderGUItext;
+        myRenderer.color = Color.white;
+        yield return new WaitForSeconds(0.05f);
+        myRenderer.material.shader = shaderSpritesDefault;
+        myRenderer.color = Color.white;
+    }
+
     public void GetHit(Projectile projectile)
     {
+        StartCoroutine(WhiteSpriteEffect());
         Damage(projectile.attackDamage);
         PushBack(projectile.transform.right, projectile.force);
 
