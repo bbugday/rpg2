14942cb565f0b0e9a34d71a925d36e2a010a3367
berke <bugdayberke@gmail.com>
2022-10-24 02:31:05 +0300

    save stat upgrades

--- Assets/Scripts/PlayerDataManager.cs
+++ Assets/Scripts/PlayerDataManager.cs
@@ -1,8 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerDataManager : Singleton<PlayerDataManager>
+public class PlayerDataManager : Singleton<PlayerDataManager>, ISavable
 {
     public Dictionary<string, PlayerStat> stats;
 
@@ -28,5 +29,41 @@ public class PlayerDataManager : Singleton<PlayerDataManager>
     public int GetCurrentUpgrade(string statName)
     {
         return stats[statName].currentUpgrade;
+    }
+
+    public object CaptureState()
+    {
+        PlayerSaveData playerSaveData = new PlayerSaveData()
+        {
+            statNames = new string[stats.Count],
+            currentUpgrades = new int[stats.Count]
+        };
+
+        int i = 0;
+        foreach(KeyValuePair<string, PlayerStat> stat in stats)
+        {
+            playerSaveData.statNames[i] = stat.Key;
+            playerSaveData.currentUpgrades[i] = stat.Value.currentUpgrade;    
+            i++;
+        }
+
+        return playerSaveData;
+    }
+
+    public void RestoreState(object state)
+    {
+        PlayerSaveData playerSaveData = (PlayerSaveData)state;
+
+        for(int i = 0; i < playerSaveData.statNames.Length; i++)
+        {
+            stats[playerSaveData.statNames[i]].currentUpgrade = playerSaveData.currentUpgrades[i];
+        }
+    }
+    
+    [Serializable]
+    public class PlayerSaveData
+    {
+        public string[] statNames;
+        public int[] currentUpgrades;
     }
 }
