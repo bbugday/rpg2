8b567a82f776a654c0d9879781b128571d0f3508
berke <bugdayberke@gmail.com>
2022-11-20 02:05:32 +0300

    enemy spawn timer

--- Assets/Scripts/Arena/Enemy/EnemySpawner.cs
+++ Assets/Scripts/Arena/Enemy/EnemySpawner.cs
@@ -5,9 +5,27 @@ using UnityEngine;
 public class EnemySpawner : MonoBehaviour
 {
     [SerializeField] List<Enemy> enemyList;
-    
+    [SerializeField] List<Enemy> spawningEnemies;
+
+    private int spawningEnemyCount = 0;
+
     void Start() 
     {
+        spawningEnemies.Add(enemyList[spawningEnemyCount++]);
+        Invoke("AddEnemy", 1 * 60f);
+        Invoke("AddEnemy", 3 * 60f);
+        Invoke("AddEnemy", 5 * 60f);
+        Invoke("AddEnemy", 10 * 60f);
+        Invoke("AddEnemy", 15 * 60f);
+        Invoke("AddEnemy", 20 * 60f);
+
+        // Invoke("AddEnemy", 1 * 40f);
+        // Invoke("AddEnemy", 1 * 40f);
+        // Invoke("AddEnemy", 1 * 40f);
+        // Invoke("AddEnemy", 10 * 60f);
+        // Invoke("AddEnemy", 15 * 60f);
+        // Invoke("AddEnemy", 20 * 60f);
+
         InvokeRepeating("SpawnEnemy", 1f, 4f);  //1s delay, repeat every 1s
     }
 
@@ -19,6 +37,13 @@ public class EnemySpawner : MonoBehaviour
 
     void SpawnEnemy() 
     {
-        Instantiate(enemyList[Random.Range(0, enemyList.Count)], gameObject.transform.position, Quaternion.identity);
+        //Instantiate(enemyList[Random.Range(0, enemyList.Count)], gameObject.transform.position, Quaternion.identity);
+        //Instantiate(enemyList[0], gameObject.transform.position, Quaternion.identity);
+        Instantiate(spawningEnemies[Random.Range(0, spawningEnemies.Count)], gameObject.transform.position, Quaternion.identity);
+    }
+
+    void AddEnemy() 
+    {
+        spawningEnemies.Add(enemyList[spawningEnemyCount++]);
     }
 }
